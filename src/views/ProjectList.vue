a6a70532e7712c673c037986b9341d705aaf4d78
lbusch25 <lbusch@macalester.edu>
2019-10-13 15:28:43 -0500

    First commit for website

---
+++ src/views/ProjectList.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="projectList">
+        <h1>This is a projects page</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+export default class ProjectList extends Vue{
+    
+}
+</script>
